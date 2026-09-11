51d98c2aa1717a68ab0c992bd883daadfc5427ef
alopezz <alopez@ecorestaura.cl>
2026-09-11 11:06:39 -0300

    dendograma horizontal

--- R/07_dendogramas.R
+++ R/07_dendogramas.R
@@ -40,20 +40,26 @@ plot_dendograma_especies <- function(hc, k = 3, metodo_dist = "bray") {
     ggplot2::geom_text(
       data = hojas,
       ggplot2::aes(x = x, y = -0.03 * y_max, label = label, color = grupo),
-      angle = 90, hjust = 1, size = 3.5, fontface = "italic"
+      hjust = 1, size = 3.5, fontface = "italic" # aca habia angle = 90 para vertical 
     ) +
     ggplot2::labs(
       title = "Similitud entre especies",
-      subtitle = paste("Distancia:", etiqueta_metodo, "— agrupadas según estaciones donde aparecen"),
       x = "", y = "Distancia"
     ) +
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0.35, 0.05))) +
+    ggplot2::coord_flip() + # horizontal
     ggplot2::theme_minimal() +
     ggplot2::theme(
-      axis.text.x = ggplot2::element_blank(),
-      axis.ticks.x = ggplot2::element_blank(),
-      panel.grid.major.x = ggplot2::element_blank(),
-      panel.grid.minor.x = ggplot2::element_blank(),
+      # dendograma horizontal:
+      axis.text.y = ggplot2::element_blank(),
+      axis.ticks.y = ggplot2::element_blank(),
+      panel.grid.major.y = ggplot2::element_blank(),
+      panel.grid.minor.y = ggplot2::element_blank(),
+      # VERTICAL:
+      # axis.text.x = ggplot2::element_blank(),
+      # axis.ticks.x = ggplot2::element_blank(),
+      # panel.grid.major.x = ggplot2::element_blank(),
+      # panel.grid.minor.x = ggplot2::element_blank(),
       legend.position = "none",
       plot.title = ggplot2::element_text(hjust = 0.5, face = "bold", size = 14),
       plot.subtitle = ggplot2::element_text(hjust = 0.5, size = 10)
